2bc88e6866237d9cd86eab3a3d62b422a344e679
kkkcw777 <107673573+kkkcw777@users.noreply.github.com>
2022-07-27 17:56:12 +0900

    메모장 쓰기 기능

--- 메모/memo.py
+++ 메모/memo.py
@@ -1,3 +1,6 @@
+from fileinput import filename
+
+
 print("hello world")
 print("진행할 작업을 선택해주세요")
 print("1. 메모 읽기 2. 메모하기")
@@ -5,10 +8,16 @@ print("1. 메모 읽기 2. 메모하기")
 option=input()
 print(option)
 if option=='1':
-    print("아직")
+    newmemo=input("메모를 입력해주세요 /n")
+    print(newmemo)
+    filename='memo.txt'
+    f=open(filename,'a')
+    f.write(newmemo)
+    f.close()
 elif option=='2':
-    f=open('memo.txt')
+    f=open('memo2.txt')
     memo=f.read()
     print(memo)
+    f.close()
 else :
     print("잘못 입력함")
